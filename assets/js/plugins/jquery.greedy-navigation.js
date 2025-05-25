161368fe2400d6572632c609583b209d251d5fee
O. Ridwan Bello <oridwanbello@gmail.com>
2025-05-25 15:20:46 -0400

    Refactor masthead navigation structure and improve theme toggle functionality

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -8,83 +8,79 @@
 var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
+var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
 var $hlinks = $('#site-nav .hidden-links');
-var $siteTitle = $('#site-nav .site-title');
-var $themeToggle = $('#theme-toggle');
 
 var breaks = [];
 
 function updateNav() {
-  var availableSpace = $nav.width()
-                      - $siteTitle.outerWidth(true)
-                      - $themeToggle.outerWidth(true)
-                      - $btn.outerWidth(true)
-                      - 20; // Safety margin
 
-  var numOfVisibleItems = $vlinks.children().length;
-  var numOfItems = numOfVisibleItems + $hlinks.children().length;
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
   // The visible list is overflowing the nav
-  if($vlinks.outerWidth(true) > availableSpace) {
-    // Record the width of the list
-    breaks.push($vlinks.outerWidth(true));
+  if ($vlinks.width() > availableSpace) {
 
-    // Move item to the hidden list
-    $vlinks.children().last().prependTo($hlinks);
-    numOfVisibleItems -= 1;
+    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
+      // Record the width of the list
+      breaks.push($vlinks.width());
 
-    // Show the dropdown btn
-    if($hlinks.children().length > 0) {
-      $btn.removeClass('hidden');
+      // Move item to the hidden list
+      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
+
+      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+      // Show the dropdown btn
+      $btn.removeClass("hidden");
     }
 
-  // The visible list is not overflowing
+    // The visible list is not overflowing
   } else {
-    // There is space for more items in the nav
-    if(availableSpace > breaks[breaks.length-1]) {
+
+    // There is space for another item in the nav
+    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
       // Move the item to the visible list
-      $hlinks.children().first().appendTo($vlinks);
+      if ($vlinks_persist_tail.children().length > 0) {
+        $hlinks.children().first().insertBefore($vlinks_persist_tail);
+      } else {
+        $hlinks.children().first().appendTo($vlinks);
+      }
       breaks.pop();
     }
 
     // Hide the dropdown btn if hidden list is empty
-    if(breaks.length < 1) {
+    if (breaks.length < 1) {
       $btn.addClass('hidden');
+      $btn.removeClass('close');
       $hlinks.addClass('hidden');
     }
   }
 
-  // Recur if the visible list is still overflowing the nav
-  if($vlinks.outerWidth(true) > availableSpace) {
-    updateNav();
-  }
+  // Keep counter updated
+  $btn.attr("count", breaks.length);
 
-  // Update counter on button
-  $btn.attr('count', numOfItems - numOfVisibleItems);
-
-  // Update masthead height and body padding
+  // update masthead height and the body/sidebar top padding
   var mastheadHeight = $('.masthead').height();
   $('body').css('padding-top', mastheadHeight + 'px');
-  if($(".author__urls-wrapper button").is(":visible")) {
+  if ($(".author__urls-wrapper button").is(":visible")) {
     $(".sidebar").css("padding-top", "");
   } else {
     $(".sidebar").css("padding-top", mastheadHeight + "px");
   }
+
 }
 
 // Window listeners
-$(window).on('resize', function() {
+
+$(window).on('resize', function () {
+  updateNav();
+});
+screen.orientation.addEventListener("change", function () {
   updateNav();
 });
 
-screen.orientation.addEventListener("change", function() {
-  updateNav();
-});
-
-$btn.on('click', function() {
+$btn.on('click', function () {
   $hlinks.toggleClass('hidden');
   $(this).toggleClass('close');
 });
 
-// Initialize
 updateNav();
