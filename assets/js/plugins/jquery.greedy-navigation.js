e05f64614c2dbbfabebb068c89616bb281d5caf5
O. Ridwan Bello <oridwanbello@gmail.com>
2025-05-25 15:14:14 -0400

    Refactor masthead navigation for improved structure and styling

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -9,55 +9,63 @@ var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
 var $hlinks = $('#site-nav .hidden-links');
+var $siteTitle = $('#site-nav .site-title');
+var $themeToggle = $('#theme-toggle');
 
 var breaks = [];
 
 function updateNav() {
-  // Get elements that should persist
-  var $logoItem = $vlinks.find('.masthead__menu-item--lg');
-  var $themeToggle = $vlinks.find('#theme-toggle');
-  var $regularItems = $vlinks.children(':not(.masthead__menu-item--lg):not(#theme-toggle)');
+  var availableSpace = $nav.width()
+                      - $siteTitle.outerWidth(true)
+                      - $themeToggle.outerWidth(true)
+                      - $btn.outerWidth(true)
+                      - 20; // Safety margin
 
-  // Calculate available space
-  var availableSpace = $nav.width() - $logoItem.outerWidth(true) - $themeToggle.outerWidth(true) - $btn.outerWidth(true) - 30;
+  var numOfVisibleItems = $vlinks.children().length;
+  var numOfItems = numOfVisibleItems + $hlinks.children().length;
 
   // The visible list is overflowing the nav
-  if ($regularItems.outerWidth(true) > availableSpace) {
+  if($vlinks.outerWidth(true) > availableSpace) {
+    // Record the width of the list
+    breaks.push($vlinks.outerWidth(true));
+
+    // Move item to the hidden list
+    $vlinks.children().last().prependTo($hlinks);
+    numOfVisibleItems -= 1;
+
     // Show the dropdown btn
-    $btn.removeClass("hidden");
-
-    while ($regularItems.outerWidth(true) > availableSpace && $regularItems.length > 0) {
-      // Record the width of the list
-      breaks.push($regularItems.outerWidth(true));
-
-      // Move item to the hidden list
-      $regularItems.last().prependTo($hlinks);
-      // Update our collection
-      $regularItems = $vlinks.children(':not(.masthead__menu-item--lg):not(#theme-toggle)');
+    if($hlinks.children().length > 0) {
+      $btn.removeClass('hidden');
     }
+
+  // The visible list is not overflowing
   } else {
     // There is space for more items in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
-      // Move the first item from hidden to visible
+    if(availableSpace > breaks[breaks.length-1]) {
+      // Move the item to the visible list
       $hlinks.children().first().appendTo($vlinks);
       breaks.pop();
     }
 
-    // Hide dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
+    // Hide the dropdown btn if hidden list is empty
+    if(breaks.length < 1) {
       $btn.addClass('hidden');
-      $btn.removeClass('close');
       $hlinks.addClass('hidden');
     }
   }
 
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
+  // Recur if the visible list is still overflowing the nav
+  if($vlinks.outerWidth(true) > availableSpace) {
+    updateNav();
+  }
+
+  // Update counter on button
+  $btn.attr('count', numOfItems - numOfVisibleItems);
 
   // Update masthead height and body padding
   var mastheadHeight = $('.masthead').height();
   $('body').css('padding-top', mastheadHeight + 'px');
-  if ($(".author__urls-wrapper button").is(":visible")) {
+  if($(".author__urls-wrapper button").is(":visible")) {
     $(".sidebar").css("padding-top", "");
   } else {
     $(".sidebar").css("padding-top", mastheadHeight + "px");
@@ -68,6 +76,7 @@ function updateNav() {
 $(window).on('resize', function() {
   updateNav();
 });
+
 screen.orientation.addEventListener("change", function() {
   updateNav();
 });
@@ -77,4 +86,5 @@ $btn.on('click', function() {
   $(this).toggleClass('close');
 });
 
+// Initialize
 updateNav();
